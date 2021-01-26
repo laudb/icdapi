98efbe5e3fed7a3e00edba9ef70b10b01b7a6374
Laud Bentil <laudbentil@gmail.com>
2021-01-26 06:11:32 +0000

    add shell command to manager

--- runner.py
+++ runner.py
@@ -6,6 +6,10 @@ from flask_migrate import MigrateCommand
 app = create_app(os.getenv('APP_ENV'))
 manager = Manager(app)
 
+def make_shell_context():
+    return dict(app=app, db=db)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
