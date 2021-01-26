2735b5a83126db69bc1d070d59254a806fa6a7e8
Laud Bentil <laudbentil@gmail.com>
2021-01-26 17:28:59 +0000

    minor formatting

--- runner.py
+++ runner.py
@@ -4,15 +4,16 @@ from app.models import Record
 from flask_script import Manager, Shell
 from flask_migrate import MigrateCommand
 
-print({'env--':os.getenv('APP_ENV') })
-app = create_app(os.getenv('APP_ENV'))
+app = create_app(os.getenv("APP_ENV"))
 manager = Manager(app)
+
 
 def make_shell_context():
     return dict(app=app, db=db, Record=Record)
 
-manager.add_command('shell', Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
 
-if __name__ == '__main__':
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
+
+if __name__ == "__main__":
     manager.run()
