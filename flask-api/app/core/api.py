a943244180553f3fbcf59818be8f058fe0e5103f
Laud Bentil <laudbentil@gmail.com>
2021-01-27 08:38:02 +0000

    add remaining endpoints, minor fixes

--- flask-api/app/core/api.py
+++ flask-api/app/core/api.py
@@ -1,3 +1,4 @@
+from flask import request,jsonify
 from . import records
 
 from app.models import Record
@@ -5,10 +6,29 @@ from app.models import Record
 
 @records.route("/status", methods=["GET"])
 def status():
-    return f"diagnose app: Version 1"
+    return f"diagnose api: Version 1"
+
+@records.route("/records", methods=["GET"])
+def show_all():
+    records = Record.query.all()
+    return records
 
 @records.route("/records", methods=["POST"])
 def create():
     payload = request.get_json()
-    result = Record.save(payload)
+    result = Record(payload)
+    result = result.save()
     return str(result)
+
+@records.route("/records/<int: id>", methods=["PUT"])
+def edit_one(id):
+    pass
+
+@records.route("/records/<int: id>", methods=["GET"])
+def get_one(id):
+    record = Record.query.get_or_404(id)
+
+@records.route("/records/<int: id>", methods=["DELETE"])
+def delete_one(id):
+    pass
+
