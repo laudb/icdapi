b9a36e5abb364b64a3c3c352918bc27e76ddeef4
Laud Bentil <laudbentil@gmail.com>
2021-01-27 09:06:23 +0000

    update test, disable createone test

--- flask-api/tests/test_app.py
+++ flask-api/tests/test_app.py
@@ -7,3 +7,18 @@ def test_status(test_client):
     expected = "diagnose app: Version 1"
     result = res.get_data().decode("utf-8")
     assert expected == result
+
+def test_not_status(test_client):
+    res = test_client.post("/api/v1/status")
+    assert res.status_code == 405
+
+# re-write create post test
+def test_post(test_client):
+    test_payload = {
+        'code': 'X00',
+        'short_desc': 'short description',
+        'long_desc': 'long long long description',
+        'type': 'ICD-10',
+        'year':'2018'
+    }
+    pass
