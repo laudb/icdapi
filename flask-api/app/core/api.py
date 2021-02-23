564a86e83299b348232d021dae593ccb128bfbef
Laud Bentil <laudbentil@gmail.com>
2021-02-23 09:57:02 +0000

    update views with http status codes, fix endpoints

--- flask-api/app/core/api.py
+++ flask-api/app/core/api.py
@@ -6,24 +6,25 @@ from app.models import Record
 
 @records.route("/status", methods=["GET"])
 def status():
-    return f"diagnose api: Version 1"
+    return f"diagnose api: Version 1", 200
 
-@records.route("/records/page/<int:id>", methods=["GET"])
-def show_all(page=1):
+@records.route("/records/page/<int:page_id>", methods=["GET"])
+def show_all(page_id=1):
     batch = 20
     records = Record.query.order_by(
         Record.code.desc()
-    ).paginate(page, per_page=batch)
-    return records
+    ).paginate(page_id, per_page=batch)
+    return records, 200
 
 @records.route("/records", methods=["POST"])
 def create():
     payload = request.get_json()
     valid = Record.is_icd_valid(payload['code'])
-    if valid:
-        result = Record(payload)
-        result = result.save()
-        return str(result)
+    if not valid:
+        return 400
+    result = Record(payload)
+    result = result.save()
+    return result.id, 201
 
 @records.route("/records/<int:id>", methods=["PUT"])
 def edit_one(id):
@@ -32,16 +33,16 @@ def edit_one(id):
     for one in data:
         record[one] = data[one] 
     result = record.save()
-    return result
+    return result.id, 201
 
 @records.route("/records/<int:id>", methods=["GET"])
 def get_one(id):
     record = Record.query.get_or_404(id)
-    return record
+    return record, 200
 
 @records.route("/records/<int:id>", methods=["DELETE"])
 def delete_one(id):
     record = Record.query.get_or_404(id)
     record.delete()
-    return id
+    return id, 204
 
