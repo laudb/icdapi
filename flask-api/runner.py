ae014091a3e861cae9649c0483e75ae4fae0a65a
Laud Bentil <laudbentil@gmail.com>
2021-02-23 09:54:23 +0000

    fix formatting

--- flask-api/runner.py
+++ flask-api/runner.py
@@ -13,6 +13,7 @@ manager = Manager(app)
 def make_shell_context():
     return dict(app=app, db=db, Record=Record)
 
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
 manager.add_command("runserver", Server(host=host, port=port))
